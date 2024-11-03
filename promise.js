29ad73ef9bffa3cc91df35b25650ee2168fb9065
saurabh989759 <saurabh98king@gmail.com>
2024-11-03 16:27:19 +0530

    new changes

---
+++ promise.js
@@ -0,0 +1,13 @@
+const cart = ["shoes" , "pants" , "kurta"]
+
+createOrder(cart , function(orderId){
+    proceedToPayment(cart , function(orderId){
+        proceedToPayment(orderId);
+    })
+});
+
+const promise = createOrder(cart)
+
+promise.then(function (orderId){
+    proceedToPayment(orderId)
+})
