e8577253fd88c6ae39fc75f55dd90965b3d46192
saurabh989759 <saurabh98king@gmail.com>
2024-11-04 10:39:35 +0530

    promise

--- promise.js
+++ promise.js
@@ -1,13 +1,41 @@
 const cart = ["shoes" , "pants" , "kurta"]
 
+// call back hell and pyramid of doom 
 createOrder(cart , function(orderId){
-    proceedToPayment(cart , function(orderId){
-        proceedToPayment(orderId);
+    proceedToPayment(orderId , function(paymentInfo){
+        showOrderSummary(paymentInfo , function(){
+            updateWalletBalance() ;
+        })
     })
 });
 
-const promise = createOrder(cart)
+//so to encounter we use promise chaining
 
-promise.then(function (orderId){
-    proceedToPayment(orderId)
+createOrder
+.then(function (orderId){
+  return   proceedToPayment(orderId)  // return is important to avoid error 
+})  
+.then(function(paymentInfo){
+  return    showOrderSummary(paymentInfo)
 })
+.then(function(){
+  return   updateWalletBalance() ; 
+})
+
+//or using arrow functions
+createOrder
+.then((orderId) => proceedToPayment(orderId) ) // return is important to avoid error 
+.then((paymentInfo) =>  showOrderSummary(paymentInfo))
+.then(() =>   updateWalletBalance() )
+
+function createOrder(cart){
+    const pr = new Promoise(resolve , reject) {
+        if(!validateCart(cart)){
+            const er = new Error("cart is not valid")
+            reject(er) ;
+        }
+        //logic to create the orderId and we created orderid 
+        return resolve(orderId) ; 
+    }
+    return pr ; 
+}
